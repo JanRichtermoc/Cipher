e8b89660ea68113509207e233ff02c7c0e387ef5
JanRichtermoc <194924887+JanRichtermoc@users.noreply.github.com>
2026-07-26 14:27:09 +0200

    Initial Commit

---
+++ Cipher/CipherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherApp.swift
+//  Cipher
+//
+//  Created by admin on 26.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CipherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
